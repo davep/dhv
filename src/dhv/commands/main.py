6f9903e746f515e70397a230f888c8f70c6391c7
Dave Pearson <davep@davep.org>
2025-10-22 11:34:55 +0100

    :lipstick: Tidy the help for various commands

--- src/dhv/commands/main.py
+++ src/dhv/commands/main.py
@@ -7,7 +7,7 @@ from textual_enhanced.commands import Command
 
 ##############################################################################
 class NewCode(Command):
-    """Empty the editor ready to enter some new code."""
+    """Empty the editor ready to enter some new code"""
 
     BINDING_KEY = "ctrl+n"
     SHOW_IN_FOOTER = True
@@ -16,7 +16,7 @@ class NewCode(Command):
 
 ##############################################################################
 class LoadFile(Command):
-    """Load the content of a Python source file."""
+    """Load the content of a Python source file"""
 
     BINDING_KEY = "ctrl+l"
     SHOW_IN_FOOTER = True
@@ -25,35 +25,35 @@ class LoadFile(Command):
 
 ##############################################################################
 class SwitchLayout(Command):
-    """Switch the screen layout between horizontal and vertical."""
+    """Switch the screen layout between horizontal and vertical"""
 
     BINDING_KEY = "f2"
 
 
 ##############################################################################
 class ShowDisassemblyOnly(Command):
-    """Only show the disassembly of the source code."""
+    """Only show the disassembly of the source code"""
 
     BINDING_KEY = "ctrl+b"
 
 
 ##############################################################################
 class ShowASTOnly(Command):
-    """Only show the AST of the source code."""
+    """Only show the AST of the source code"""
 
     BINDING_KEY = "ctrl+t"
 
 
 ##############################################################################
 class ShowDisassemblyAndAST(Command):
-    """Show both the disassembly and the AST of the source code."""
+    """Show both the disassembly and the AST of the source code"""
 
     BINDING_KEY = "ctrl+o"
 
 
 ##############################################################################
 class ChangeCodeTheme(Command):
-    """Change the theme of the Python code editor."""
+    """Change the theme of the Python code editor"""
 
     BINDING_KEY = "ctrl+f9"
 
